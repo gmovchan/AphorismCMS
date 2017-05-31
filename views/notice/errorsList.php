778491e3f288ab8dbb3f726a48f42b9ef77ce935
grigoryMovchan <grigory08@ya.ru>
2017-05-31 11:08:46 +0300

    Update errorsList.php

--- views/notice/errorsList.php
+++ views/notice/errorsList.php
@@ -1,10 +1,8 @@
 <div class="bg-danger errors">Произошли ошибки:
     <ul>
-        <?php if (!is_null($data['errors'])):
-            foreach ($data['errors'] as $key => $value): ?>
-                <li><?php echo $value; ?></li>
-            <?php endforeach; ?>
-        <?php endif; ?>
+        <?php foreach ($data['errors'] as $key => $value): ?>
+            <li><?php echo $value; ?></li>
+        <?php endforeach; ?>
     </ul>
 </div>
 

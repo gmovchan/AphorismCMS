9daabe1c48f257f0cb6d6150e10736a77703a3f8
grigoryMovchan <grigory08@ya.ru>
2017-05-31 11:09:22 +0300

    Update successfulList.php

--- views/notice/successfulList.php
+++ views/notice/successfulList.php
@@ -1,10 +1,8 @@
 <div class="bg-success successful">Успешно выполнено:
     <ul>
-        <?php if (!is_null($data['successful'])):
-            foreach ($data['successful'] as $key => $value): ?>
-                <li><?php echo $value; ?></li>
-            <?php endforeach; ?>
-        <?php endif; ?>
+        <?php foreach ($data['successful'] as $key => $value): ?>
+            <li><?php echo $value; ?></li>
+        <?php endforeach; ?>
     </ul>
 </div>
 
